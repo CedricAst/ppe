c3854adf64013ca27323e2bd630b63101f21a5c4
Utilisateur <astier50cedric@gmail.com>
2020-01-24 11:53:20 +0100

    envoie2

---
+++ html/pageaccueil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="icon" type="image/png" href="images/icon.png">
+		<link rel="stylesheet" type="text/css" href="css/general.css" media="all"> 
+		<link rel="stylesheet" type="text/css" href="css/index.css" media="all"> 
+		<title>Redout</title>
+	</head>
+	
+	<body>
+		<div class="navigation">
+			<img class="navigation-logo photo" src="logo.png" alt="">
+			<div class="navigation-links">
+				<label>Redout</label>
+			</div>
+		</div>
