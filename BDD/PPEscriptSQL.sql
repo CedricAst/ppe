1ba7c767f286f0ed673f1dddf082ed1605ceb128
Pickwi <william.bonnemoy@gmail.com>
2020-01-30 10:49:06 +0100

    script de la BDD OK

---
+++ BDD/PPEscriptSQL.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE BDD_PPE1
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE BDD_PPE1 ;
+
+CREATE TABLE Bannit 
+(
+idBannit INT NOT NULL AUTO_INCREMENT,
+pseudoBannit VARCHAR(50),
+justification VARCHAR(50),
+idProfile INT,
+PRIMARY KEY (idBannit)
+);
+
+CREATE TABLE Profile 
+(
+idProfile INT NOT NULL AUTO_INCREMENT,
+pseudo VARCHAR(50),
+MDP VARCHAR(50),
+grade VARCHAR(50),
+PRIMARY KEY (idProfile)
+);
+
+CREATE TABLE Sujet
+(
+idSujet INT NOT NULL AUTO_INCREMENT,
+nomSujet VARCHAR(50),
+likeSujet INT,
+dislikeSujet INT,
+text TEXT,
+idProfile INT,
+PRIMARY KEY (idSujet)
+);
+
+CREATE  TABLE Message 
+(
+idMessage INT NOT NULL AUTO_INCREMENT,
+text TEXT,
+likeMessage INT,
+dislikeMessage INT,
+idSujet INT,
+PRIMARY KEY (idMessage)
+);
+
+ALTER TABLE Bannit
+ADD CONSTRAINT Bannit_idProfile
+FOREIGN KEY (idProfile) 
+REFERENCES Profile(idProfile);
+
+ALTER TABLE Sujet
+ADD CONSTRAINT Sujet_idProfile 
+FOREIGN KEY (idProfile) 
+REFERENCES Profile(idProfile);
+
+ALTER TABLE Message
+ADD CONSTRAINT Message_idSujet
+FOREIGN KEY (idSujet) 
+REFERENCES Sujet(idSujet);
