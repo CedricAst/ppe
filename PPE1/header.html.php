1726267396b0a1fb37539c73a9fdcec2508e0a51
Utilisateur <astier50cedric@gmail.com>
2020-01-31 11:51:31 +0100

    sauvegarde 31/10/2020

---
+++ PPE1/header.html.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="icon" type="image/png" href="images/icon.png">
+        <link rel="stylesheet" type="text/css" href="general.css" media="all">
+        <title>Redout</title>
+    </head>
+    <body>
+        <div class="navigation">
+            <img class="navigation-logo photo" src="logo.png" alt="">
+            <div class="navigation-links">
+                <label>Redout</label> 
+            </div>
+        </div>
+        <div class="header">
+        </div>
+
+<?php
+
+?>
+
+
+
