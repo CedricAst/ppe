681c68db7ca6e6bb6e349c27d4be39da2c1b6592
Pickwi <william.bonnemoy@gmail.com>
2020-01-31 11:27:04 +0100

    header+dossier imglogo

--- PPE1/header.php
+++ PPE1/header.php
@@ -1,23 +1,9 @@
 <!DOCTYPE html>
-<head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="icon" type="image/png" href="images/icon.png">
-        <link rel="stylesheet" type="text/css" href="general.css" media="all">
-        <title>Redout</title>
-    </head>
-    <body>
-        <div class="navigation">
-            <img class="navigation-logo photo" src="logo.png" alt="">
-            <div class="navigation-links">
-                <label>Redout</label> 
-            </div>
-        </div>
-        <div class="header">
-        </div>
-
-<?php
-
-?>
-
-
-
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>REDOUT</title>
+        <link rel="icon" type="image/png" href="image/logo.png" />
+        <link rel="stylesheet" type="text/css" href="css/general.css" media="all"/>
+	</head>
+	<body>
