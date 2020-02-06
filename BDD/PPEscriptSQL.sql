a36bf94b058e19999fbcf2bdd16af3d6bf32144c
Pickwi <william.bonnemoy@gmail.com>
2020-02-06 09:05:37 +0100

    sql MAJ

--- BDD/PPEscriptSQL.sql
+++ BDD/PPEscriptSQL.sql
@@ -7,18 +7,18 @@ CREATE TABLE Bannit
 idBannit INT NOT NULL AUTO_INCREMENT,
 pseudoBannit VARCHAR(50),
 justification VARCHAR(50),
-idProfile INT,
+idUtilisateur INT,
 PRIMARY KEY (idBannit)
 );
 
 CREATE TABLE Utilisateur
 (
-idProfile INT NOT NULL AUTO_INCREMENT,
-pseudo VARCHAR(50),
+idUtilisateur INT NOT NULL AUTO_INCREMENT,
+pseudo VARCHAR(50) UNIQUE,
 MDP VARCHAR(50),
 grade VARCHAR(50),
 URLimageProfile VARCHAR(50),
-PRIMARY KEY (idProfile)
+PRIMARY KEY (idUtilisateur)
 
 );
 
@@ -29,7 +29,7 @@ nomSujet VARCHAR(50),
 likeSujet INT,
 dislikeSujet INT,
 text TEXT,
-idProfile INT,
+idUtilisateur INT,
 PRIMARY KEY (idSujet)
 );
 
@@ -45,16 +45,57 @@ PRIMARY KEY (idMessage)
 );
 
 ALTER TABLE Bannit
-ADD CONSTRAINT Bannit_idProfile
-FOREIGN KEY (idProfile) 
-REFERENCES Profile(idProfile);
+ADD CONSTRAINT Bannit_idUtilisateur
+FOREIGN KEY (idUtilisateur) 
+REFERENCES Utilisateur(idUtilisateur);
 
 ALTER TABLE Sujet
-ADD CONSTRAINT Sujet_idProfile 
-FOREIGN KEY (idProfile) 
-REFERENCES Profile(idProfile);
+ADD CONSTRAINT Sujet_idUtilisateur 
+FOREIGN KEY (idUtilisateur) 
+REFERENCES Utilisateur(idUtilisateur);
 
 ALTER TABLE Message
 ADD CONSTRAINT Message_idSujet
 FOREIGN KEY (idSujet) 
 REFERENCES Sujet(idSujet);
+/* cree un utilisateur */
+INSERT INTO Utilisateur (idUtilisateur,pseudo,MDP,grade,URLimageProfile) VALUES
+(null,"superkiller",123456789,0,"DATA\image\image_Utilisateur")
+
+/* banne un utilisateur */
+INSERT INTO Bannit (idBannit,pseudoBannit,justification,idUtilisateur) VALUES
+(null,"superkiller","pas bien",null)
+
+/* cree un Sujet */
+INSERT INTO Sujet  (idSujet,nomSujet,likeSujet,dislikeSujet,text,idUtilisateur) VALUES
+(null,"IOS<android",0,0,"voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",null)
+
+/* cree un Message */
+INSERT INTO Message (idMessage,text,likeMessage,dislikeMessage,URLimage,idSujet) VALUES
+(null,"blablabla gfdsfdf ",0,0,"DATA\image\image-message",null)
+
+/* like un sujet */
+UPDATE Sujet SET likeSujet=likeSujet+1
+WHERE idSujet=1;
+
+/* dislike un sujet */
+UPDATE Sujet SET dislikeSujet=dislikeSujet+1
+WHERE idSujet=1;
+
+/* like un Message */
+UPDATE Message SET likeMessage=likeMessage+1
+WHERE idMessage=1;
+
+/* dislike un Message */
+UPDATE Message SET dislikeMessage=dislikeMessage+1
+WHERE idMessage=1;
+
+/* change MDP*/
+UPDATE Utilisateur SET MDP="password"
+WHERE idUtilisateur=1;
+
+
+
+
+
+
